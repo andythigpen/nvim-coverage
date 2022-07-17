5481d705348c11f6e6a4fa1f414a3887314c87f4
Andrew Thigpen <andyt05@gmail.com>
2022-07-17 15:47:43 -0400

    Fix package in file names.

--- lua/coverage/languages/go.lua
+++ lua/coverage/languages/go.lua
@@ -16,18 +16,42 @@ M.summary = common.summary
 -- and https://github.com/golang/go/blob/master/src/testing/cover.go#L102
 local line_re = "^(.+):(%d+)%.%d+,(%d+)%.%d+ (%d+) (%d+)$"
 
+-- for parsing the module name from go.mod
+local mod_name_re = "^module (.*)$"
+
+local get_module_name = function()
+	local p = Path:new("go.mod")
+	if not p:exists() then
+		return ""
+	end
+	local lines = p:readlines()
+	for _, line in ipairs(lines) do
+		if line:match(mod_name_re) then
+			local name = line:match(mod_name_re)
+			name = name:gsub("%-", "%%-")
+			name = name:gsub("%.", "%%.")
+			name = name:gsub("%+", "%%+")
+			name = name:gsub("%?", "%%?")
+			return name
+		end
+	end
+	return ""
+end
+
 --- Parses a coverprofile formatted file
 --- @param path Path
 --- @param files table<string, CoverageFile>
 local parse_coverprofile = function(path, files)
 	local lines_by_filename = {}
 	local lines = path:readlines()
+	local mod_name = get_module_name()
 	for _, line in ipairs(lines) do
 		if line:match("mode:.*") then
 			-- do nothing
 		elseif line:match(line_re) then
 			-- example/main.go:3.14,5.2 0 0
 			local fname, line_start, line_end, _, count = line:match(line_re)
+			fname = fname:gsub(mod_name .. "/", "", 1)
 			line_start = tonumber(line_start)
 			line_end = tonumber(line_end)
 			count = tonumber(count)
