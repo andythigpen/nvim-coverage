7680f9235844940464682c664e828b20ff4b4c52
safoh <safoh@student.codam.nl>
2023-02-26 21:44:27 +0100

    Alias from cpp

--- lua/coverage/languages/c.lua
+++ lua/coverage/languages/c.lua
@@ -1,28 +1,18 @@
 local M = {}
 
-local Path = require("plenary.path")
-local common = require("coverage.languages.common")
-local config = require("coverage.config")
-local util = require("coverage.util")
+-- Javascript and Typescript currently use the exact same configuration.
+local cpp = require("coverage.languages.cpp")
+
+--- Use the same configuration as javascript
+M.config_alias = "cpp"
 
 --- Returns a list of signs to be placed.
-M.sign_list = common.sign_list
+M.sign_list = cpp.sign_list
 
 --- Returns a summary report.
-M.summary = common.summary
+M.summary = cpp.summary
 
 --- Loads a coverage report.
--- @param callback called with the results of the coverage report
-M.load = function(callback)
-    local c_config = config.opts.lang.cpp
-    c_config.coverage_file = "/home/safoh/repos/minishell/build/coverage.info"
-    local p = Path:new(c_config.coverage_file)
-    if not p:exists() then
-        vim.notify("No coverage file exists.", vim.log.levels.INFO, {p})
-        return
-    end
-
-    callback(util.lcov_to_table(p))
-end
+M.load = cpp.load
 
 return M
