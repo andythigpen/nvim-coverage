beb984984b0c0e64b64847d2433a1c1c96416b08
Oscar Creator <oscar.creator13@gmail.com>
2023-06-11 15:08:03 +0200

    Coverage support for lua

---
+++ lua/coverage/languages/lua.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+local Path = require("plenary.path")
+local common = require("coverage.languages.common")
+local config = require("coverage.config")
+local util = require("coverage.util")
+
+M.sign_list = common.sign_list
+
+M.summary = common.summary
+
+M.load = function(callback)
+    local lua_config = config.opts.lang.lua
+    local p = Path:new(lua_config.coverage_file)
+    if not p:exists() then
+        vim.notify("No coverage file exists.", vim.log.levels.INFO)
+    end
+    callback(util.lcov_to_table(p))
+end
+
+return M
