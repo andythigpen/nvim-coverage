42eb08eec941b34b17a86ad65f7dd69061484c35
Hoang Nguyen <folliekazetani@protonmail.com>
2023-10-21 00:00:00 +0700

    fix: only set the highlight groups as default
    This allows users to customize the highlight groups without it being
    overwridden by the plugin on load.

--- lua/coverage/highlight.lua
+++ lua/coverage/highlight.lua
@@ -16,10 +16,10 @@ local highlight = function(group, color)
     local fg = color.fg and "guifg=" .. color.fg or "guifg=NONE"
     local bg = color.bg and "guibg=" .. color.bg or "guibg=NONE"
     local sp = color.sp and "guisp=" .. color.sp or ""
-    local hl = "highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp
+    local hl = "highlight default " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp
     vim.cmd(hl)
     if color.link then
-        vim.cmd("highlight! link " .. group .. " " .. color.link)
+        vim.cmd("highlight default link " .. group .. " " .. color.link)
     end
 end
 
